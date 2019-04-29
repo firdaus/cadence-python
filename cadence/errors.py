b28272073e967d5c0d9edf94d5c887d5b32d60d6
Mohammed Firdaus <firdaus.halim@gmail.com>
2019-04-30 04:31:49 +0800

    Map error fields to error objects.

---
+++ cadence/errors.py
@@ -0,0 +1,117 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class BadRequestError:
+    message: str
+
+
+@dataclass
+class InternalServiceError:
+    message: str
+
+
+@dataclass
+class DomainAlreadyExistsError:
+    message: str
+
+
+@dataclass
+class WorkflowExecutionAlreadyStartedError:
+    message: Optional[str]
+    startRequestId: Optional[str]
+    runId: Optional[str]
+
+    @property
+    def start_request_id(self):
+        return self.startRequestId
+
+    @property
+    def run_id(self):
+        return self.runId
+
+
+@dataclass
+class EntityNotExistsError:
+    message: str
+
+
+@dataclass
+class ServiceBusyError:
+    message: str
+
+
+@dataclass
+class CancellationAlreadyRequestedError:
+    message: str
+
+
+@dataclass
+class QueryFailedError:
+    message: str
+
+
+@dataclass
+class DomainNotActiveError:
+    message: str
+    domainName: str
+    currentCluster: str
+    activeCluster: str
+
+    @property
+    def domain_name(self):
+        return self.domainName
+
+    @property
+    def current_cluster(self):
+        return self.currentCluster
+
+    @property
+    def active_cluster(self):
+        return self.activeCluster
+
+
+@dataclass
+class LimitExceededError:
+    message: str
+
+
+@dataclass
+class AccessDeniedError:
+    message: str
+
+
+@dataclass
+class RetryTaskError:
+    message: str
+
+
+CADENCE_ERROR_FIELDS = {
+    "badRequestError": BadRequestError,
+    "internalServiceError": InternalServiceError,
+    "domainExistsError": DomainAlreadyExistsError,
+    "sessionAlreadyExistError": WorkflowExecutionAlreadyStartedError,
+    "entityNotExistError": EntityNotExistsError,
+    "serviceBusyError": ServiceBusyError,
+    "cancellationAlreadyRequestedError": CancellationAlreadyRequestedError,
+    "queryFailedError": QueryFailedError,
+    "domainNotActiveError": DomainNotActiveError,
+    "limitExceededError": LimitExceededError,
+}
+
+IGNORE_FIELDS_IN_ERRORS = ("args", "type_spec", "from_primitive", "to_primitive", "with_traceback")
+
+
+def find_error(response):
+    for key, cls in CADENCE_ERROR_FIELDS.items():
+        error = getattr(response, key, None)
+        if error:
+            kwargs = {}
+            for field in dir(error):
+                if field not in IGNORE_FIELDS_IN_ERRORS and not field.startswith("__"):
+                    kwargs[field] = getattr(error, field)
+            return cls(**kwargs)
+    return None
---
+++ cadence/tests/test_errors.py
@@ -0,0 +1,28 @@
+from unittest import TestCase
+
+from cadence.errors import find_error, InternalServiceError, WorkflowExecutionAlreadyStartedError
+from cadence.workflowservice import cadence
+
+
+class TestError(TestCase):
+
+    def setUp(self) -> None:
+        self.internalServiceError = cadence.shared.InternalServiceError("ERROR")
+        self.sessionAlreadyExistError = cadence.shared.WorkflowExecutionAlreadyStartedError("ERROR", "REQUEST-ID",
+                                                                                            "RUN-ID")
+
+    def test_internal_server_error(self):
+        response = cadence.WorkflowService.StartWorkflowExecution.response(
+            internalServiceError=self.internalServiceError)
+        error = find_error(response)
+        self.assertIsInstance(error, InternalServiceError)
+        self.assertEqual("ERROR", error.message)
+
+    def test_session_already_exists_error(self):
+        response = cadence.WorkflowService.StartWorkflowExecution.response(
+            sessionAlreadyExistError=self.sessionAlreadyExistError)
+        error = find_error(response)
+        self.assertIsInstance(error, WorkflowExecutionAlreadyStartedError)
+        self.assertEqual("ERROR", error.message)
+        self.assertEqual("REQUEST-ID", error.start_request_id)
+        self.assertEqual("RUN-ID", error.run_id)
