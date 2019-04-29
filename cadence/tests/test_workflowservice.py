0daf5a02c85202da90158ffd2e952b7d274cc58d
Mohammed Firdaus <firdaus.halim@gmail.com>
2019-04-30 04:38:21 +0800

    Implement WorkflowService::start_workfow()

---
+++ cadence/tests/test_workflowservice.py
@@ -0,0 +1,30 @@
+from unittest import TestCase
+from uuid import uuid4
+
+from cadence.errors import WorkflowExecutionAlreadyStartedError
+from cadence.workflowservice import WorkflowService
+
+
+class TestStartWorkflow(TestCase):
+
+    def setUp(self) -> None:
+        self.service = WorkflowService.create("localhost", 7933)
+
+    def test_start_workflow(self):
+        (runId, err) = self.service.start_workflow("test-domain", "test-tasklist", "firdaus-workflow-type",
+                                            input_value="abc-firdaus")
+        self.assertIsNotNone(runId)
+        self.assertIsInstance(runId, str)
+
+    def test_duplicate_workflow_ids(self):
+        workflow_id = str(uuid4())
+        (runId, err) = self.service.start_workflow("test-domain", "test-tasklist", "firdaus-workflow-type",
+                                            input_value="abc-firdaus", workflow_id=workflow_id)
+        (runId, err) = self.service.start_workflow("test-domain", "test-tasklist", "firdaus-workflow-type",
+                                            input_value="abc-firdaus", workflow_id=workflow_id)
+        self.assertIsNotNone(err)
+        self.assertIsNone(runId)
+        self.assertIsInstance(err, WorkflowExecutionAlreadyStartedError)
+
+    def tearDown(self) -> None:
+        self.service.connection.close()
---
+++ cadence/workflowservice.py
@@ -0,0 +1,55 @@
+from __future__ import annotations
+from uuid import uuid4
+
+import thriftrw
+import os
+import socket
+
+from cadence.connection import TChannelConnection, ThriftFunctionCall
+from cadence.errors import find_error
+
+this_dir = os.path.dirname(__file__)
+cadence_thrift = os.path.join(this_dir, "thrift/cadence.thrift")
+cadence = thriftrw.load(cadence_thrift)
+
+TCHANNEL_SERVICE = "cadence-frontend"
+
+
+class WorkflowService:
+
+    @classmethod
+    def create(cls, host: str, port: int):
+        connection = TChannelConnection.open(host, port)
+        return cls(connection)
+
+    def __init__(self, connection: TChannelConnection):
+        self.connection = connection
+        self.execution_start_to_close_timeout_seconds = 86400
+        self.task_start_to_close_timeout_seconds = 120
+        self.identity = "%d@%s" % (os.getpid(), socket.gethostname())
+
+    def start_workflow(self, domain, task_list, workflow_type_name, input_value=None, workflow_id=None):
+        start_request = cadence.shared.StartWorkflowExecutionRequest()
+        start_request.requestId = str(uuid4())
+        start_request.domain = domain
+        start_request.input = input_value
+        start_request.taskList = cadence.shared.TaskList()
+        start_request.taskList.name = task_list
+        start_request.taskList.kind = cadence.shared.TaskListKind.NORMAL
+        if not workflow_id:
+            workflow_id = str(uuid4())
+        start_request.workflowId = workflow_id
+        start_request.workflowType = cadence.shared.WorkflowType()
+        start_request.workflowType.name = workflow_type_name
+        start_request.executionStartToCloseTimeoutSeconds = self.execution_start_to_close_timeout_seconds
+        start_request.taskStartToCloseTimeoutSeconds = self.task_start_to_close_timeout_seconds
+
+        request = cadence.WorkflowService.StartWorkflowExecution.request(startRequest=start_request)
+        request_payload = cadence.dumps(request)
+
+        call = ThriftFunctionCall.create(TCHANNEL_SERVICE, "WorkflowService::StartWorkflowExecution", request_payload)
+        response = self.connection.call_function(call)
+        start_response = cadence.loads(cadence.WorkflowService.StartWorkflowExecution.response, response.thrift_payload)
+        if not start_response.success:
+            return (None, find_error(start_response))
+        return (start_response.success.runId, None)
