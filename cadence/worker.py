3326834f90a9a1df2d29f235a90d971023b4ed3e
Mohammed Firdaus <firdaus.halim@gmail.com>
2019-05-19 03:03:13 +0800

    Allow Workers to be stopped gracefully.

--- cadence/worker.py
+++ cadence/worker.py
@@ -3,6 +3,7 @@ from typing import Callable, Dict, Tuple
 import inspect
 import threading
 import logging
+import time
 
 from cadence.conversions import camel_to_snake, snake_to_camel
 from cadence.workflowservice import WorkflowService
@@ -25,6 +26,9 @@ class Worker:
     activities: Dict[str, Callable] = field(default_factory=dict)
     workflow_methods: Dict[str, Tuple[type, Callable]] = field(default_factory=dict)
     service: WorkflowService = None
+    threads_started: int = 0
+    threads_stopped: int = 0
+    stop_requested: bool = False
 
     def register_activities_implementation(self, activities_instance: object, activities_cls_name: str = None):
         cls_name = activities_cls_name if activities_cls_name else type(activities_instance).__name__
@@ -46,9 +50,26 @@ class Worker:
     def start(self):
         from cadence.activity_loop import activity_task_loop
         from cadence.decision_loop import decision_task_loop
+        self.threads_stopped = 0
+        self.threads_started = 0
+        self.stop_requested = False
         if self.activities:
             thread = threading.Thread(target=activity_task_loop, args=(self,))
             thread.start()
+            self.threads_started += 1
         if self.workflow_methods:
             thread = threading.Thread(target=decision_task_loop, args=(self,))
             thread.start()
+            self.threads_started += 1
+
+    def stop(self):
+        self.stop_requested = True
+        while self.threads_stopped != self.threads_started:
+            time.sleep(5)
+
+    def is_stop_requested(self):
+        return self.stop_requested
+
+    def notify_thread_stopped(self):
+        self.threads_stopped += 1
+
