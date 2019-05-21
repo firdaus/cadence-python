149b2f39375f0ce55de63b5b6b6f987b7989b205
Mohammed Firdaus <firdaus.halim@gmail.com>
2019-05-22 03:47:23 +0800

    Update workflow sample to invoke GreetingActivities.

--- cadence/samples/workflow_worker.py
+++ cadence/samples/workflow_worker.py
@@ -1,16 +1,28 @@
 import logging
 
+from cadence.activity_method import activity_method
 from cadence.workerfactory import WorkerFactory
-from cadence.workflow import workflow_method
+from cadence.workflow import workflow_method, Workflow
 
 logging.basicConfig(level=logging.DEBUG)
 
 
+# Interface for activity Implemented in Java
+class GreetingActivities:
+    @activity_method(task_list="HelloActivity", schedule_to_close_timeout_seconds=2)
+    def composeGreeting(self, greeting: str, name: str) -> str:
+        pass
+
+
 class GreetingWorkflowImpl:
 
-    @workflow_method(name='GreetingWorkflow::getGreeting')
+    def __init__(self):
+        self.greeting_activities = Workflow.new_activity_stub(GreetingActivities)
+
+    @workflow_method(name='GreetingWorkflow::getGreeting', impl=True)
     async def get_greeting(self, name):
-        return f"Hello, return value from Python {name}"
+        greeting = await self.greeting_activities.composeGreeting("Hello", name)
+        return f"Got from Java {greeting}"
 
 
 if __name__ == '__main__':
