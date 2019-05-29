0dbd7df28606e1af92f79d9d2a86fcc014cd520b
Mohammed Firdaus <firdaus.halim@gmail.com>
2019-05-29 09:05:29 +0800

    Add dependencies to setup.py.

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/firdaus/cadence-python",
     packages=setuptools.find_packages(exclude=["cadence.tests", "cadence.spikes"]),
+    install_requires=["more-itertools>=7.0.0", "thriftrw>=1.7.2"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
