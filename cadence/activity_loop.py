dd54a4870d75fe02d64f2e67b42daf862cf94df9
Christoffer Möckelind <christoffer.mockelind@formulate.app>
2019-08-12 17:20:42 +0200

    addressed feedback

--- cadence/activity_loop.py
+++ cadence/activity_loop.py
@@ -1,8 +1,7 @@
 import datetime
 import logging
-import json
 from typing import NoReturn, Type, Sequence, Dict
-
+import json
 from cadence.cadence_types import PollForActivityTaskRequest, TaskListMetadata, TaskList, PollForActivityTaskResponse, \
     RespondActivityTaskCompletedRequest, RespondActivityTaskFailedRequest
 from cadence.workflowservice import WorkflowService
@@ -31,24 +30,21 @@ def activity_task_loop(worker):
             polling_start = datetime.datetime.now()
             try:
                 task = service.poll_for_activity_task(polling_request)
-            except RuntimeError as ex:
-                logger.error("PollForActivityTask RuntimeError: %s", ex)
-                continue
             except Exception as ex:
                 logger.error("PollForActivityTask error: %s", ex)
                 continue
             polling_end = datetime.datetime.now()
             polling_total = polling_end - polling_start
-            logger.debug(
+            logger.info(
                 f"PollForActivityTask: done. Start: {polling_start.total_seconds()}, End: {polling_end.total_seconds()}, Total: {polling_total.total_seconds()}"
             )
 
             # Process activity task
             try:
-                ret = handle_task(task,worker.activities)
+                ret = worker.run_task(task)
                 response = RespondActivityTaskCompletedRequest(
                     task_token=task.task_token,
-                    result=json.dumps(ret),
+                    result=ret,
                     identity=WorkflowService.get_identity(),
                 )
                 service.respond_activity_task_completed(response)
@@ -63,7 +59,7 @@ def activity_task_loop(worker):
                     identity=WorkflowService.get_identity(),
                     details=json.dumps({
                         "detailMessage":
-                        f"Python error: {type(ex).__name__}({ex})"
+                        {"python_error": f"{type(ex).__name__}({ex})"}
                     }))
                 try:
                     service.respond_activity_task_failed(response)
@@ -76,37 +72,3 @@ def activity_task_loop(worker):
         logger.fatal(f"activity_task_loop: Uncought exception in main loop.",exec_info=True)
     finally:
         worker.notify_thread_stopped()
-
-
-
-
-
-def handle_task(task, activities) -> Dict:
-    def handle_error(msg: str, exception: Type[Exception]) -> NoReturn:
-        logger.error(msg)
-        raise exception(msg)
-
-    # TODO: finish this one
-    if not task.task_token:
-        handle_error("task.task_token was not provided, but is expected.",
-                     RuntimeError)
-    if task.activity_type.name not in activities:
-        handle_error(
-            f"handle_task: Activity type {task.activity_type.name} not found",
-            RuntimeError)
-    fn = activities[task.activity_type.name]
-    try:
-        args = json.loads(task.input)
-    except json.JSONDecodeError as ex:
-        handle_error(f"handle_task: Json decoding failed: {ex}", RuntimeError)
-
-    if not isinstance(args,Sequence):
-        handle_error(f"handle_task: Args should be a Sequence but where {type(args)}. args=\n{args}",RuntimeError)
-    
-    logger.debug(f"handle_task: Calling activity fn with args:\n%s",args)
-    try:
-        ret_val= fn(*args)
-    except Exception as ex:
-        handle_error(f"handle_task: Exception when running activity. Exception:\n{ex}",RuntimeError)
-    logger.debug(f"handle_task: Activity fn successfully returned. Result:\n%s",ret_val)
-    return ret_val
--- cadence/worker.py
+++ cadence/worker.py
@@ -4,10 +4,11 @@ import inspect
 import threading
 import logging
 import time
-
+from typing import NoReturn, Type, Sequence
 from cadence.conversions import camel_to_snake, snake_to_camel
 from cadence.workflowservice import WorkflowService
-
+import json
+from cadence.cadence_types import PollForActivityTaskResponse
 logger = logging.getLogger(__name__)
 
 
@@ -24,29 +25,41 @@ class Worker:
     task_list: str = None
     options: WorkerOptions = None
     activities: Dict[str, Callable] = field(default_factory=dict)
-    workflow_methods: Dict[str, Tuple[type, Callable]] = field(default_factory=dict)
+    workflow_methods: Dict[str, Tuple[type, Callable]] = field(
+        default_factory=dict)
     service: WorkflowService = None
     threads_started: int = 0
     threads_stopped: int = 0
     stop_requested: bool = False
 
-    def register_activities_implementation(self, activities_instance: object, activities_cls_name: str = None):
-        cls_name = activities_cls_name if activities_cls_name else type(activities_instance).__name__
-        for method_name, fn in inspect.getmembers(activities_instance, predicate=inspect.ismethod):
+    def register_activities_implementation(self,
+                                           activities_instance: object,
+                                           activities_cls_name: str = None):
+        cls_name = activities_cls_name if activities_cls_name else type(
+            activities_instance).__name__
+        for method_name, fn in inspect.getmembers(activities_instance,
+                                                  predicate=inspect.ismethod):
             if method_name.startswith("_"):
                 continue
             self.activities[f'{cls_name}::{camel_to_snake(method_name)}'] = fn
             self.activities[f'{cls_name}::{snake_to_camel(method_name)}'] = fn
 
-    def register_workflow_implementation_type(self, cls: type, workflow_cls_name: str = None):
+    def register_workflow_implementation_type(self,
+                                              cls: type,
+                                              workflow_cls_name: str = None):
         cls_name = workflow_cls_name if workflow_cls_name else cls.__name__
-        for method_name, fn in inspect.getmembers(cls, predicate=inspect.isfunction):
+        for method_name, fn in inspect.getmembers(
+                cls, predicate=inspect.isfunction):
             if hasattr(fn, "_workflow_method") and fn._workflow_method:
                 self.workflow_methods[fn._name] = (cls, fn)
                 if "::" in fn._name:
                     _, method_name = fn._name.split("::")
-                    self.workflow_methods[f'{cls_name}::{camel_to_snake(method_name)}'] = (cls, fn)
-                    self.workflow_methods[f'{cls_name}::{snake_to_camel(method_name)}'] = (cls, fn)
+                    self.workflow_methods[
+                        f'{cls_name}::{camel_to_snake(method_name)}'] = (cls,
+                                                                         fn)
+                    self.workflow_methods[
+                        f'{cls_name}::{snake_to_camel(method_name)}'] = (cls,
+                                                                         fn)
 
     def start(self):
         from cadence.activity_loop import activity_task_loop
@@ -55,7 +68,7 @@ class Worker:
         self.threads_started = 0
         self.stop_requested = False
         if self.activities:
-            thread = threading.Thread(target=activity_task_loop, args=(self,))
+            thread = threading.Thread(target=activity_task_loop, args=(self, ))
             thread.start()
             self.threads_started += 1
         if self.workflow_methods:
@@ -74,3 +87,44 @@ class Worker:
     def notify_thread_stopped(self):
         self.threads_stopped += 1
 
+    def run_task(self, task : PollForActivityTaskResponse) -> str:
+        def handle_error(msg: str, exception: Type[Exception]) -> NoReturn:
+            logger.error(msg)
+            raise exception(msg)
+
+        if not task.task_token:
+            handle_error("task.task_token was not provided, but is expected.",
+                         RuntimeError)
+        if task.activity_type.name not in self.activities:
+            handle_error(
+                f"Worker.run_task : Activity type {task.activity_type.name} not found",
+                RuntimeError)
+        fn = self.activities[task.activity_type.name]
+        try:
+            args = json.loads(task.input)
+        except json.JSONDecodeError as ex:
+            handle_error(f"Worker.run_task : Json decoding failed: {ex}",
+                         RuntimeError)
+
+        if not isinstance(args, Sequence):
+            handle_error(
+                f"Worker.run_task : Args should be a Sequence but where {type(args)}. args=\n{args}",
+                RuntimeError)
+
+        logger.debug(f"Worker.run_task : Calling activity fn with args:\n%s", args)
+        try:
+            ret_val = fn(*args)
+        except Exception as ex:
+            handle_error(
+                f"Worker.run_task : Exception when running activity. Exception:\n{ex}",
+                RuntimeError)
+        logger.debug(
+            f"Worker.run_task : Activity fn successfully returned. Result:\n%s",
+            ret_val)
+        try:
+            serialized_ret_val = json.dumps(ret_val)
+        except TypeError as ex:
+            msg = f"Worker.run_task : Failed to serialize output. Result:\n%s\nException: %s"
+            logger.error(msg,ret_val,exc_info=True)
+            raise RuntimeError(msg)
+        return serialized_ret_val
