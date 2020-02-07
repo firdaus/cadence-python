9cf905b3f32d61450945cb66576a7eaf095c7431
Mohammed Firdaus <firdaus.halim@gmail.com>
2020-02-08 02:20:52 +0800

    Add Exception types of workflow and activity failures that contain the context of the exception.

--- cadence/exceptions.py
+++ cadence/exceptions.py
@@ -1,4 +1,6 @@
-from cadence.cadence_types import TimeoutType
+from dataclasses import dataclass
+
+from cadence.cadence_types import TimeoutType, ActivityType, WorkflowExecution
 
 
 class IllegalStateException(BaseException):
@@ -50,3 +52,39 @@ class CancellationException(Exception):
 
 class ActivityCancelledException(Exception):
     pass
+
+
+@dataclass
+class WorkflowOperationException(Exception):
+    event_id: int = None
+
+
+@dataclass
+class ActivityException(WorkflowOperationException):
+    activity_type: ActivityType = None
+    activity_id: str = ""
+
+    def __str__(self):
+        return f'{type(self).__name__}  ActivityType="{self.activity_type.name}", ActivityID="{self.activity_id}", ' \
+               f'EventID={self.event_id} '
+
+
+@dataclass
+class ActivityFailureException(ActivityException):
+    attempt: int = None
+    backoff: int = 0
+
+
+@dataclass
+class WorkflowException(Exception):
+    workflow_type: str = None
+    execution: WorkflowExecution = None
+
+    def __str__(self):
+        return f'{type(self).__name__}: WorkflowType="{self.workflow_type}", ' \
+               f'WorkflowID="{self.execution.workflow_id}", RunID="{self.execution.run_id} '
+
+
+@dataclass
+class WorkflowFailureException(WorkflowException):
+    decision_task_completed_event_id: int = None
