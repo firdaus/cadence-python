1ebfbf05d5a25dc38865f70aec81cf2e7781dbb3
Mohammed Firdaus <firdaus.halim@gmail.com>
2020-03-27 23:26:44 +0800

    Released beta1.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cadence-client",
-    version="0.0.4",
+    version="1.0.0-beta1",
     author="Mohammed Firdaus",
     author_email="firdaus.halim@gmail.com",
     description="Python framework for Cadence Workflow Service",
