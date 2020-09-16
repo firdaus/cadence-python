cfd7a48e6da7c289c9ae0c29c94d12d2b05986e4
Chandan Bhattad <cbhattad@noon.com>
2020-09-16 19:50:55 +0400

    putting in try catch

--- cadence/activity_loop.py
+++ cadence/activity_loop.py
@@ -39,9 +39,12 @@ def activity_task_loop(worker: Worker):
             except Exception as ex:
                 logger.error("PollForActivityTask error: %s", ex)
                 # possible that connection with cadence server broke, so recreating
-                service: WorkflowService = WorkflowService.create(worker.host, worker.port,
+                try:
+                    service: WorkflowService = WorkflowService.create(worker.host, worker.port,
                                                                   timeout=worker.get_timeout())
-                worker.manage_service(service)
+                    worker.manage_service(service)
+                except Exception as ex:
+                    logger.error(f"Could not create workflow service due to: {ex}")
                 continue
             if err:
                 logger.error("PollForActivityTask failed: %s", err)
